bbc06971e37f849812dd6661df57ccce4ffc4237
Scott Sanders <ssanders3@wsgc.com>
2018-11-16 17:40:15 -0800

    body parse fix

--- auth/login/index.js
+++ auth/login/index.js
@@ -3,41 +3,43 @@ const parse = require('urlencoded-body-parser');
 const users = require('./users');
 
 module.exports = (req, res) => {
-  // POST /auth/logout
-  // make sure we have a body to process
-  const data = cookie.parse(req);
-  console.log(`attempting login: ${data.username}`);
-  if (!data || !data.username) return res.end(400);
+  // POST /auth/login
   const validOrigins = ['https://testauth.net', 'https://testpb.net', 'https://testws.net'];
   const origin = req.url.origin;
   if (req.method === 'POST') {
     if (origin && validOrigins.includes(origin)) {
-      //we have a valid CORS request, continue
-      res.setHeader('Cache-Control', 'max-age=0');
-      res.setHeader('Access-Control-Allow-Origin', origin);
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-      const username = data.username;
-      if (users[username] && users[username].profile) {
-        users[username].profile.tag = new Date().toString();
-        console.dir(profile);
-      }
-      var profile = users[username].profile
-      if (profile) {
-        res.setHeader('Set-Cookie', cookie.serialize('auth', JSON.stringify(profile), {
-          domain: '.testauth.net',
-          httpOnly: true,
-          path: "/",
-          secure: true,
-          maxAge: 60 * 60 * 24 * 7 // 1 week
-        }));
+      const data = parse(req).then(data => {
+        // make sure we have a body to process
+        console.log(`attempting login: ${data.username}`);
+        if (!data || !data.username) return res.end(400);
+      
+        //we have a valid CORS request, continue
+        res.setHeader('Cache-Control', 'max-age=0');
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+        const username = data.username;
+        if (users[username] && users[username].profile) {
+          users[username].profile.tag = new Date().toString();
+          console.dir(profile);
+        }
+        var profile = users[username].profile
+        if (profile) {
+          res.setHeader('Set-Cookie', cookie.serialize('auth', JSON.stringify(profile), {
+            domain: '.testauth.net',
+            httpOnly: true,
+            path: "/",
+            secure: true,
+            maxAge: 60 * 60 * 24 * 7 // 1 week
+          }));
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          return res.end(JSON.stringify(profile));
+        }
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        return res.end(JSON.stringify(profile));
-      }
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      return res.end('{anonymous: true}');
+        return res.end('{anonymous: true}');
+      });
     } else {
       //this is an invalid CORS request (no origin)
       res.statusCode = 500;
