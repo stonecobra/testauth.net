ba08564c01e48c64bbc31764247db21f779fdcbf
Scott Sanders <ssanders3@wsgc.com>
2018-11-16 17:46:59 -0800

    origin fix

--- auth/login/index.js
+++ auth/login/index.js
@@ -5,8 +5,10 @@ const users = require('./users');
 module.exports = (req, res) => {
   // POST /auth/login
   const validOrigins = ['https://testauth.net', 'https://testpb.net', 'https://testws.net'];
-  const origin = req.url.origin;
+  const origin = req.headers['origin'];
+  console.log(`origin: ${origin}`);
   if (req.method === 'POST') {
+    console.log('request is a POST request', req.url);
     if (origin && validOrigins.includes(origin)) {
       const data = parse(req).then(data => {
         // make sure we have a body to process
